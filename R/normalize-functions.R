7da38a15bdd52a990a7b9ca942d9209f3902a980
Julian Gehring <julian.gehring@fdm.uni-freiburg.de>
2011-05-02 13:25:49 +0200

    correct ".normalize" averaging
    use averaged values for calculation of lambda only
    clean up code

--- R/normalize-functions.R
+++ R/normalize-functions.R
@@ -14,45 +14,42 @@
     rle <- rle(start)
     ind2 <- cumsum(rle$lengths)
     ind1 <- c(1L, ind2[-length(ind2)]+1L)
-    #.nRep <- function(x) length(unique(x))
-    start <- start[ind1]
-    end <- end[ind1]
     mReads <- sapply(1:length(ind1), .normDup, y=nReads, i1=ind1, i2=ind2, fun=fun)
     nRep <- rle$lengths
-    #strand0 <- sapply(i=1:length(ind1), normDup, y=nReads0, i1=ind1, i2=ind2, fun=fun)
-    #replicate0 <- sapply(i=1:length(ind1), normDup, y=nReads0, i1=ind1, i2=ind2, fun=fun)
   } else {
     mReads <- nReads
     nRep <- rep(1L, length(mReads))
   }
-  nData <- length(mReads)
 
   ## add basal offsets
-  indEst <- (1L:nData) + offset
-  #pos <- c(start[1L]+(-offset:-1L), start, start[length(start)]+(1L:offset))
-  longReads <- rep(basal, nData+2*offset)
+  m <- length(mReads)
+  indEst <- (1L:m) + offset
+  longReads <- rep(basal, m+2*offset)
   longReads[indEst] <- mReads
 
   ## Poisson ratio
   lambda0 <- .initialFun(longReads, ratio, basal)[indEst]
+  lambda <- rep(lambda0, nRep)
 
   ## store results from Poisson ratio
-  res <- data.frame(start=start, end=end, mReads=mReads, lambda0=lambda0, nRep=nRep)
+  res <- data.frame(start=start, end=end, nReads=nReads, lambda=lambda)
 
   if(fit == TRUE) {
     ## Poisson fit
-    rOpt <- optim(fn=.assess, gr=.assessGrad, par=lambda0, method="L-BFGS-B",
-                  lower=rep(basal, nData), control=list(trace=0, maxit=500), ## CHCK: maxit
-                  nReads=mReads, regpara=regpara, basal=basal, nRep=nRep) ## TODO: arg names
-    rTrust <- bobyqa(fn=.assess, par=lambda0, lower=rep(basal, nData),
-                     upper=Inf, control=list(iprint=0, maxfun=10*nData^2), ## CHCK: maxfun
-                     nReads=mReads, regpara=regpara, basal=basal, nRep=NULL) ## TODO: arg names
+    n <- length(lambda)
+    lower <- rep(basal, n)
+    rOpt <- optim(fn=.assess, gr=.assessGrad, par=lambda, method="L-BFGS-B",
+                  lower=lower, control=list(trace=0, maxit=500), ## CHCK: maxit
+                  nReads=nReads, regpara=regpara, basal=basal, nRep=nRep) ## TODO: arg names
+    rTrust <- bobyqa(fn=.assess, par=lambda, lower=lower,
+                     control=list(iprint=0, maxfun=10*n^2), ## CHCK: maxfun
+                     nReads=nReads, regpara=regpara, basal=basal, nRep=NULL) ## TODO: arg names
     par <- if(rTrust$fval > rOpt$value) rOpt$par else rTrust$par
     optim <- if(rTrust$fval > rOpt$value) 1L else 2L
 
     ## store results from fit
     res$pReads <- par
-    res$optim <- rep(optim, nData)
+    res$optim <- rep(optim, n)
   }
 
   return(res)
