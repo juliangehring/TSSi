e7dea255b1c054fc0de1603dfef7fc213c30a765
Julian Gehring <julian.gehring@fdm.uni-freiburg.de>
2011-05-26 15:40:39 +0200

    fix bugs in "plot" method
    add automatic ylim
    rearrange argument handling

--- R/plot-functions.R
+++ R/plot-functions.R
@@ -13,114 +13,87 @@
   if(missing(y) || !is.data.frame(reads))
     stop("'y' must be specified.")
 
-  ## plot
-  plot1 <- list(x=reads$start, y=reads$counts, type="n") ## TODO better
-  plot3 <- list(xlab="Position", ylab="Reads", main=segmentName)
+  ## gather arguments
+  thresholdArgs <- if(threshold)
+    .getArgs("thresholdArgs", args,
+             list(h=parameters(x, "threshold")),
+             list(col="gray"))
+
+  rugArgs <- if(rug)
+    .getArgs("rugArgs", args,
+             list(x=tss(x, y)$pos))
   
-  do.call("plot", .getArgs("plotArgs", plot1, plot3, args))
+  expectArgs <- if(expect)
+    .getArgs("expectArgs", args,
+             list(x=start, y=reads$expect),
+             list(pch=24, col=4, type="b", lwd=1, lty=1))
+
+  countsArgs <- if(counts)
+    .getArgs("countsArgs", args,
+             list(x=start, y=reads$counts),
+             list(pch=21, col=1, lwd=1, lty=1, type="p"))
   
-  ## threshold
-  if(threshold) {
-    threshold1 <- list(h=parameters(x, "threshold"))
-    threshold3 <- list(col="gray")
-    thresholdArgs <- .getArgs("thresholdArgs", threshold1, threshold3, args)
-    
-    do.call("abline", thresholdArgs)
-  }
+  ratioArgs <- if(ratio)
+    .getArgs("ratioArgs", args,
+             list(x=start, y=reads$ratio),
+             list(pch=22, col=2, lwd=1, lty=1, type="p"))
 
-  ## rug
-  if(rug) {
-    tssData <- tss(x, y)
-    rug1 <- list(x=tssData$pos)
-    rugArgs <- .getArgs("rugArgs", rug1, list(), args)
-    
-    do.call("rug", rugArgs)
-  }
+  fitArgs <- if(fit <- fit && "fit" %in% names(reads))
+    .getArgs("fitArgs", args,
+             list(x=start, y=reads$fit),
+             list(pch=20, col=3, lwd=1, lty=1, type="p"))
 
-  ## expect
-  expectArgs <- NULL
-  if(expect) {
-    expect1 <- list(x=start, y=reads$expect)
-    expect3 <- list(pch=24, col=4, type="b", lwd=1, lty=1)
-    expectArgs <- .getArgs("expectArgs", expect1, expect3, args)
-    
-    do.call("points", expectArgs)
-  }  
-
-  ## tss
-  tssArgs <- NULL
-  if(tss) {
-    tssData <- tss(x, y)
-    tss1 <- list(x=tssData$pos, y=tssData$reads)
-    tss3 <- list(pch=20, lwd=2, col=1, lty=1)
-    tssArgs <- .getArgs("tssArgs", tss1, tss3, args)
-    
-    do.call("points", c(type="h", tssArgs))
-    do.call("points", c(type="p", tssArgs))
-    
-    tssArgs <- c(tssArgs, type="h") ## for legend
-  }
-
-  ## counts
-  countsArgs <- NULL
-  if(counts) {
-    counts1 <- list(x=start, y=reads$counts)
-    counts3 <- list(pch=21, col=1, lwd=1, lty=1, type="p")
-    countsArgs <- .getArgs("countsArgs", counts1, counts3, args)
-    
-    do.call("points", countsArgs)
-  }
-
-  ## ratio
-  ratioArgs <- NULL
-  if(ratio) {
-    ratio1 <- list(x=start, y=reads$ratio)
-    ratio3 <- list(pch=22, col=2, lwd=1, lty=1, type="p")
-    ratioArgs <- .getArgs("ratioArgs", ratio1, ratio3, args)
-    
-    do.call("points", ratioArgs)
-  }  
-
-  ## fit
-  fitArgs <- NULL
-  if(fit <- fit && "fit" %in% names(reads)) {
-    fit1 <- list(x=start, y=reads$fit)
-    fit3 <- list(pch=20, col=3, lwd=1, lty=1, type="p")
-    fitArgs <- .getArgs("fitArgs", fit1, fit3, args)
-    
-    do.call("points", fitArgs)
-  }
+  tssArgs <- if(tss)
+    .getArgs("tssArgs", args,
+             list(x=tss(x, y)$pos, y=tss(x, y)$reads),
+             list(pch=20, lwd=2, col=1, lty=1))
 
   ## legend
-  if(legend) {
+  if(legend && any(ind <- c(expect, counts, ratio, fit, tss))) {
     lArgs <- list(expectArgs, countsArgs, ratioArgs, fitArgs, tssArgs)
-    
-    ind <- c(expect, counts, ratio, fit, tss)
     varNames <- c("expect", "counts", "ratio", "fit", "tss")
     isLine <- !(.catLegend("type", lArgs) %in% c("p", "n"))
-
     legend1 <- list(legend=varNames[ind],
                     col=.catLegend("col", lArgs),
                     pch=.catLegend("pch", lArgs),
                     lwd=ifelse(isLine, .catLegend("lwd", lArgs), NA),
                     lty=ifelse(isLine, .catLegend("lty", lArgs), NA))
     legend3 <- list(x="topleft")
-    legendArgs <- .getArgs("legendArgs", legend1, legend3, args)
-
-    do.call("legend", legendArgs)
+    legendArgs <- .getArgs("legendArgs", args, legend1, legend3)
   }
+
+  ylim <- c(0, max(.catLegend("y", lArgs[ind])))
+  plotArgs <- .getArgs("plotArgs", args,
+                       list(x=start, y=reads$counts, type="n"),
+                       list(xlab="Position", ylab="Reads", ylim=ylim, main=segmentName))
+
+  ## plot
+  do.call("plot", plotArgs)
+  if(threshold) do.call("abline", thresholdArgs)
+  if(rug) do.call("rug", rugArgs)
+  if(expect) do.call("points", expectArgs)
+  if(tss) {    
+    do.call("points", c(type="h", tssArgs))
+    do.call("points", c(type="p", tssArgs))
+    tssArgs <- c(tssArgs, type="h") ## for legend
+  }
+  if(counts) do.call("points", countsArgs)
+  if(ratio) do.call("points", ratioArgs)
+  if(fit) do.call("points", fitArgs)
+
+  do.call("legend", legendArgs)
 }
 
 
 ## getArgs ##
-.getArgs <- function(name, first=NULL, last=NULL, ...) {
+.getArgs <- function(name, args, first=NULL, last=NULL) {
 
-  ind <- which(names(...) %in% name)[1] ## [[]] allows only one element for indexing
-  middle <- if(length(ind) != 0) ...[[ind]] else NULL
-  args <- c(first, middle, last)
-  args <- args[!duplicated(names(args))]
+  ind <- which(names(args) %in% name)[1] ## [[]] allows only one element for indexing
+  middle <- if(length(ind) != 0) args[[ind]] ## NULL else
+  res <- c(first, middle, last)
+  res <- res[!duplicated(names(res))]
   
-  return(args)
+  return(res)
 }
 
 
