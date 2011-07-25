0e9cc995399ec4b1c227dc05ad6f26e616d444e9
Julian Gehring <julian.gehring@fdm.uni-freiburg.de>
2011-07-25 14:21:33 +0200

    register C functions probably

---
+++ src/assess.h
@@ -0,0 +1,7 @@
+#include <R.h>
+
+void assessAbs(double *ratio, double *basal, int *nin, double *ass);
+void assessAbsGrad(double *ratio, double *basal, int *nin, double *dass);
+void assessSteps(double *ratio2, int *len2, double *ass);
+void assessStepsGrad(double *ratio2, int *len2, double *dass);
+
